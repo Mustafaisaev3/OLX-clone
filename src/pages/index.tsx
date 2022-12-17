67f6f12e54a1be6ee247d6c10b1d4a9a8c01f045
Isaev Mustafa <mustafaisaev3@gmail.com>
2022-12-17 16:18:27 +0300

    Add Home Search section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,3 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styled from 'styled-components'
 import Header from '../components/Header/Header'
 import HomeSearch from '../components/HomeSearch/HomeSearch'
 
